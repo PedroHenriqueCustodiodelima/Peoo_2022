2dca44f32919929976e4a28eab5a7c6815e31832
Pedro Henrique Custódio de lima <101941886+PedroHenriqueCustodiodelima@users.noreply.github.com>
2022-06-08 23:13:13 +0000

    Lista_6

---
+++ Lista 6/Ex_1116.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+class Program{
+static void Main(string[] args){
+  int a = int.Parse(Console.ReadLine());
+  for (int i = 0; i < a; i++){
+        string[] vet = Console.ReadLine().Split(' ');
+        int x = int.Parse(vet[0]);
+        int y = int.Parse(vet[1]);
+        if (y == 0){
+          Console.WriteLine("divisao impossivel");
+        }
+        else{
+          double div = (double)x / y;
+          Console.WriteLine(div.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+  }
+}
