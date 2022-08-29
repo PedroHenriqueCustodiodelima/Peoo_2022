7b795a367145067d2eec38d6dcf828ce0ec51b28
Pedro Henrique Custódio de lima <101941886+PedroHenriqueCustodiodelima@users.noreply.github.com>
2022-08-29 20:24:42 +0000

    peoo

--- Peoo_att/ex01.cs
+++ Peoo_att/ex01.cs
@@ -22,10 +22,11 @@ using System;
 class Program {
   static void Main() {  
     imc imc = new imc();
-    Console.WriteLine("Digite o seu peso");
+    Console.WriteLine("Digite o seu peso em kg:");
     imc.SetPeso(double.Parse(Console.ReadLine()));
-    Console.WriteLine("Digite sua altura");
+    Console.WriteLine("Digite sua altura em metros");
     imc.SetAltura(double.Parse(Console.ReadLine()));
     Console.WriteLine($"Seu imc é: {imc.CalcIMC():0.00}");
+    Console.WriteLine("IMC é: O índice de massa corporal é uma medida internacional usada para calcular se uma pessoa está no peso idea");
     }
 }
